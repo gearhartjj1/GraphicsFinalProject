ebd92ea1b48bd06dc1a7b3e07d04f98df3b2c124
zachjwheeler <zachw.foss@gmail.com>
2014-04-25 18:08:47 -0400

    Add intersection testing stuff

---
+++ Final Project/Intersection Testing/main.cpp
@@ -0,0 +1,9 @@
+#include "tests.h"
+#include <iostream>
+
+int main(int argc, char** argv) {
+	RunTests();
+
+	system("PAUSE");
+	return 0;
+}
---
+++ Final Project/Intersection Testing/stubs.cpp
@@ -0,0 +1,24 @@
+#include "stubs.h"
+
+using namespace glm;
+
+double Test_RaySphereIntersect(const vec3& P0, const vec3& V0, const mat4& T) {
+	// TODO fill this in.
+	// See the documentation of this function in stubs.h.
+
+	return -1;
+}
+
+double Test_RayPolyIntersect(const vec3& P0, const vec3& V0, const vec3& p1, const vec3& p2, const vec3& p3, const mat4& T) {
+	// TODO fill this in.
+	// See the documentation of this function in stubs.h.
+
+	return -1;
+}
+
+double Test_RayCubeIntersect(const vec3& P0, const vec3& V0, const mat4& T) {
+	// TODO fill this in.
+	// See the documentation of this function in stubs.h.
+
+	return -1;
+}
