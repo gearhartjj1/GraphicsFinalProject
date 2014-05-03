2e6633091ece4a6f409ffc587e56b9b8349e8db8
zachjwheeler <zachw.foss@gmail.com>
2014-05-03 12:39:26 -0400

    Fix sphere geometry

--- Final Project/Program1/Sphere.cpp
+++ Final Project/Program1/Sphere.cpp
@@ -44,58 +44,65 @@ Sphere::Sphere() {
 	};
 	
 	//bottom
-	f[0]->indices[0] = 0;
-	f[0]->indices[1] = 3;
-	f[0]->indices[2] = 2;
-	f[0]->indices[3] = 1;
+	f[0]->indices[3] = 0;
+	f[0]->indices[2] = 3;
+	f[0]->indices[1] = 2;
+	f[0]->indices[0] = 1;
 	
 	//top
-	f[1]->indices[0] = 4;
-	f[1]->indices[1] = 7;
-	f[1]->indices[2] = 6;
-	f[1]->indices[3] = 5;
+	f[1]->indices[3] = 4;
+	f[1]->indices[2] = 7;
+	f[1]->indices[1] = 6;
+	f[1]->indices[0] = 5;
 	
 	//left
-	f[2]->indices[0] = 0;
-	f[2]->indices[1] = 1;
-	f[2]->indices[2] = 6;
-	f[2]->indices[3] = 7;
+	f[2]->indices[3] = 0;
+	f[2]->indices[2] = 1;
+	f[2]->indices[1] = 6;
+	f[2]->indices[0] = 7;
 	
 	//front
-	f[3]->indices[0] = 3;
-	f[3]->indices[1] = 0;
-	f[3]->indices[2] = 7;
-	f[3]->indices[3] = 4;
+	f[3]->indices[3] = 3;
+	f[3]->indices[2] = 0;
+	f[3]->indices[1] = 7;
+	f[3]->indices[0] = 4;
 
 	//right
-	f[4]->indices[0] = 2;
-	f[4]->indices[1] = 3;
-	f[4]->indices[2] = 4;
-	f[4]->indices[3] = 5;
+	f[4]->indices[3] = 2;
+	f[4]->indices[2] = 3;
+	f[4]->indices[1] = 4;
+	f[4]->indices[0] = 5;
 
 	//back
-	f[5]->indices[0] = 1;
-	f[5]->indices[1] = 2;
-	f[5]->indices[2] = 5;
-	f[5]->indices[3] = 6;
+	f[5]->indices[3] = 1;
+	f[5]->indices[2] = 2;
+	f[5]->indices[1] = 5;
+	f[5]->indices[0] = 6;
 
 	mesh.getVertices().resize(8);
 	for(int i=0; i < 8; ++i)
 		mesh.getVertices()[i] = new Vertex(v[i], getColor(), n[i]);
 	mesh.getFaces().assign(f, f+6);
 
-	catmullclark(mesh, 1);
+	catmullclark(mesh, 3);
 
 	//until catmullclark runs
 	//mesh.setFilled(true);
 	//mesh.triangulate();
 
-	//find effective radius and scale
-	/*float r = 0;
+	//find average radius, scale, translate down
+	float r = 0;
+	float hi=0,lo=200;
+	for(int i=0; i < (int)mesh.getVertices().size(); ++i) {
+		hi = glm::max(hi, mesh.getVertices()[i]->position.y);
+		lo = glm::min(lo, mesh.getVertices()[i]->position.y);
+	}
 	for(int i=0; i < (int)mesh.getVertices().size(); ++i)
-		r = glm::max(r, glm::length(glm::vec3(mesh.getVertices()[i]->position) - glm::vec3(0.f, .5f, 0.f)));
+		r += glm::length(glm::vec3(mesh.getVertices()[i]->position) - glm::vec3(0.f, .5f*(hi-lo), 0.f));
+	r /= mesh.getVertices().size();
 	for(int i=0; i < (int)mesh.getVertices().size(); ++i)
-		mesh.getVertices()[i]->position = glm::scale(glm::mat4(1.f), glm::vec3(1/r, 1/r, 1/r)) * mesh.getVertices()[i]->position;*/
+		mesh.getVertices()[i]->position = glm::scale(glm::mat4(1.f), glm::vec3(.5/r, .5/r, .5/r)) * 
+				glm::translate(glm::mat4(1.f), glm::vec3(0.f,-lo,0.f)) * mesh.getVertices()[i]->position;
 }
 
 void Sphere::draw(glm::mat4 transform, glm::vec3 color)
